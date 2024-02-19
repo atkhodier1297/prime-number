0f5cee93a0074e5508831d9c787f4748d396d8fa
Adam Khodier <khodieradam1297@gmail.com>
2024-02-19 10:29:07 -0500

    solved

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 // Problem: Given an integer 'n', determine if its a prime number.
 // Prime numbers can only be multiplied by 1 and itself.
 
+function isPrime(n){
+    if(n < 2) {
+        return false
+    }
+    for(let i = 2; i < n; i++){
+        if(n % i === 0) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(isPrime(5))
+console.log(isPrime(15))
+console.log(isPrime(20))
